b910cff6397e7b012c58b96d807deaa646c0f847
sawrus@gmail.com <sawrus@gmail.com@7159ea72-5042-624f-90be-b02e59218691>
2012-02-21 10:44:32 +0000

    Support fill model from context

--- trunk/src/com/solutions/samples/mvc/controllers/impl/StudentController.java
+++ trunk/src/com/solutions/samples/mvc/controllers/impl/StudentController.java
@@ -1,27 +1,26 @@
 package com.solutions.samples.mvc.controllers.impl;
 
 import com.solutions.samples.mvc.controllers.AbstractController;
-import com.solutions.samples.mvc.entities.impl.Group;
-import com.solutions.samples.mvc.entities.impl.Student;
 import com.solutions.samples.mvc.events.Event;
 import com.solutions.samples.mvc.events.impl.StudentEvent;
 import com.solutions.samples.mvc.models.impl.StudentModel;
 import com.solutions.samples.mvc.views.impl.StudentConsoleView;
 
 import java.io.IOException;
-import java.util.Date;
 
 public class StudentController extends AbstractController<StudentModel, StudentConsoleView> {
     private final EventHandler studentEventHandler = new EventHandler() {
-        private Student entity;
         public void handle(Event event) {
-            entity = model.getEntity();
             switch ((StudentEvent)event){
                 case CLEAR:
-                    entity.clear();
+                    model.clear();
                     break;
                 case FILL:
-                    fillStudent();
+                    try {
+                        fillStudent();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                     break;
                 case SHOW:
                     view.show();
@@ -31,19 +30,10 @@ public class StudentController extends AbstractController<StudentModel, StudentC
             }
         }
 
-        private void fillStudent() {
-            try {
-                view.fill();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            entity.setGroup((Group) context.getProperty("group"));
-            entity.setStartLearn((Date) context.getProperty("startLearn"));
-
-            entity.setName(String.valueOf(view.context.getProperty("name")));
-            entity.setLastName(String.valueOf(view.context.getProperty("lastName")));
-            entity.setSecondName(String.valueOf(view.context.getProperty("secondName")));
+        private void fillStudent() throws IOException {
+            view.fill();
+            model.fillPrimaryInformation(context);
+            model.fillSecondaryInformation(view.context);
         }
     };
 
--- trunk/src/com/solutions/samples/mvc/models/impl/StudentModel.java
+++ trunk/src/com/solutions/samples/mvc/models/impl/StudentModel.java
@@ -1,6 +1,21 @@
 package com.solutions.samples.mvc.models.impl;
 
+import com.solutions.samples.mvc.context.IContext;
+import com.solutions.samples.mvc.entities.impl.Group;
 import com.solutions.samples.mvc.entities.impl.Student;
 import com.solutions.samples.mvc.models.AbstractModel;
 
-public class StudentModel extends AbstractModel<Student> { }
+import java.util.Date;
+
+public class StudentModel extends AbstractModel<Student> {
+    public void fillPrimaryInformation(IContext context){
+        entity.setGroup((Group) context.getProperty("group"));
+        entity.setStartLearn((Date) context.getProperty("startLearn"));
+    }
+
+    public void fillSecondaryInformation(IContext context){
+        entity.setName(String.valueOf(context.getProperty("name")));
+        entity.setLastName(String.valueOf(context.getProperty("lastName")));
+        entity.setSecondName(String.valueOf(context.getProperty("secondName")));
+    }
+}
