b700d3e15e4c187f55fc9314be0cb49443d0f3f0
alekseyportnow@gmail.com <alekseyportnow@gmail.com@7159ea72-5042-624f-90be-b02e59218691>
2012-03-21 15:34:23 +0000

    Абстрактный класс модели

---
+++ branches/nape/src/com/solutions/models/AbstractModel.java
@@ -0,0 +1,24 @@
+package com.solutions.mvc.models;
+
+import com.solutions.mvc.entities.IEntity;
+
+public abstract class AbstractModel<TEntity extends IEntity> implements IModel<TEntity>{
+    protected TEntity entity;
+    protected IModel parent;
+
+    public void setParent(IModel parent) {
+        this.parent = parent;
+    }
+
+    protected AbstractModel(TEntity entity) {
+        this.entity = entity;
+    }
+
+    public void clear() {
+        entity.clear();
+    }
+
+    public TEntity getEntity() {
+        return entity;
+    }
+}
