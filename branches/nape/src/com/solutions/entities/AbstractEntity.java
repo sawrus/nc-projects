7d3cfb1ea17d9089608b147c324530bca491e747
alekseyportnow@gmail.com <alekseyportnow@gmail.com@7159ea72-5042-624f-90be-b02e59218691>
2012-03-21 15:28:07 +0000

    Абстрактный класс сущности

---
+++ branches/nape/src/com/solutions/entities/AbstractEntity.java
@@ -0,0 +1,27 @@
+package com.solutions.mvc.entities;
+
+public abstract class AbstractEntity implements IEntity{
+    protected String name;
+
+    protected AbstractEntity() {
+    }
+
+    protected AbstractEntity(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "AbstractEntity{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
