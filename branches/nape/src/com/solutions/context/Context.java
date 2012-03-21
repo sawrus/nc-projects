c08fa3c59f354e7c3472e4096d6b448baff1537c
alekseyportnow@gmail.com <alekseyportnow@gmail.com@7159ea72-5042-624f-90be-b02e59218691>
2012-03-21 15:17:03 +0000

    Класс контекста

---
+++ branches/nape/src/com/solutions/context/Context.java
@@ -0,0 +1,16 @@
+package com.solutions.mvc.context;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Context implements IContext{
+    private final Map<String, Object> properties = new HashMap<String, Object>();
+
+    public void setProperty(String name, Object value) {
+        properties.put(name, value);
+    }
+
+    public Object getProperty(String name) {
+        return properties.get(name);
+    }
+}
