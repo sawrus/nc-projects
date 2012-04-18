3dcb292cbe3e6d46e092052fb083b9a8b8ea6352
alekseyportnow@gmail.com <alekseyportnow@gmail.com@7159ea72-5042-624f-90be-b02e59218691>
2012-04-18 21:41:51 +0000

    Отформатировал код с помощью IDE

--- src/com/solutions/entities/impl/Agency.java
+++ src/com/solutions/entities/impl/Agency.java
@@ -1,51 +1,54 @@
 package com.solutions.entities.impl;
 
-import java.util.*;
-
 import com.solutions.entities.AbstractEntity;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 
 public class Agency extends AbstractEntity {
 
- private HashMap<String, Depart> agency = new HashMap<String, Depart>();
+    private HashMap<String, Depart> agency = new HashMap<String, Depart>();
 
- public Agency(){}
-
- public Depart GetDepart(String name){
-     return agency.get(name);
- }
-
- public void SetDepart (Depart depart){
-  agency.put(depart.getName(), depart);
-  }
-
- public void clear() {
-    name = "";
-		agency.clear();
+    public Agency() {
     }
 
- public void DeleteDepart (String name){
-  agency.remove(name);
-  }
+    public Depart GetDepart(String name) {
+        return agency.get(name);
+    }
 
- public Depart CheckDepart(String title) {
+    public void SetDepart(Depart depart) {
+        agency.put(depart.getName(), depart);
+    }
+
+    public void clear() {
+        name = "";
+        agency.clear();
+    }
+
+    public void DeleteDepart(String name) {
+        agency.remove(name);
+    }
+
+    public Depart CheckDepart(String title) {
         Collection<Depart> dep = Values();
         Iterator<Depart> itr = dep.iterator();
         while (itr.hasNext()) {
             Depart element = itr.next();
             if (element.getName().compareTo(title) == 0) {
-               return element;
+                return element;
             }
         }
-           Depart element = new Depart();
-           element.setName(title);
-           SetDepart(element);
-           return element;
- }
+        Depart element = new Depart();
+        element.setName(title);
+        SetDepart(element);
+        return element;
+    }
 
 
- public Collection<Depart> Values(){
-     return agency.values();
- }
+    public Collection<Depart> Values() {
+        return agency.values();
+    }
 
 
 }
