346d68ca91d13606b3ba98874a8ea440bda249e4
alekseyportnow@gmail.com <alekseyportnow@gmail.com@7159ea72-5042-624f-90be-b02e59218691>
2012-04-23 19:07:37 +0000

    улучшенная версия XML

--- src/com/solutions/XML/XML.java
+++ src/com/solutions/XML/XML.java
@@ -7,53 +7,63 @@ import com.solutions.entities.impl.Vicar;
 import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.Collection;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
-
-
 public class XML {
 
-public static void XML_Save(Office Pers, String Filename) throws Exception {
-     Collection<Vicar> itogo = Pers.Values();
+    public static void XML_Save(Office pers, String filename) throws Exception {
+        Collection<Vicar> itogo = pers.Values();
         try {
-            FileOutputStream out = new FileOutputStream(Filename);
+            FileOutputStream out = new FileOutputStream(filename);
             GZIPOutputStream gz = new GZIPOutputStream(out);
             XMLEncoder xmlEncoder = new XMLEncoder(gz);
-            for (Vicar Obj : itogo) {
-                xmlEncoder.writeObject(Obj);
+            for (Vicar obj : itogo) {
+                xmlEncoder.writeObject(obj);
             }
             xmlEncoder.flush();
             xmlEncoder.close();
         } catch (java.io.EOFException eofe) {
-            System.out.println("????? ???? ?????????? ??? ???");
+            System.out.println("File is empty");
+        } catch (FileNotFoundException e) {
+            System.out.println("There are not file" + filename);
+            System.exit(0);
         }
     }
 
-public static void XML_Load(Office Pers, Agency Dep, String Filename) throws Exception {
+    public static void XML_Load(Office pers, Agency dep, String filename) throws Exception {
         try {
-            FileInputStream in = new FileInputStream(Filename);
+            FileInputStream in = new FileInputStream(filename);
             GZIPInputStream gs = new GZIPInputStream(in);
             XMLDecoder xmlDecoder = new XMLDecoder(gs);
-            Vicar Obj = new Vicar();
+            Vicar obj = new Vicar();
             while (true) {
                 try {
-                    Obj = (Vicar) xmlDecoder.readObject();
+                    obj = (Vicar) xmlDecoder.readObject();
                 } catch (ArrayIndexOutOfBoundsException e) {
-                    System.out.println("????? ???? ?????????? ??? ???");
+                    System.out.println("It is end of file");
                     break;
                 }
-                Pers.SetVicar(Obj);
+                pers.SetVicar(obj);
                 try {
-                    Dep.SetDepart(Obj.getDepart());
+                    dep.SetDepart(obj.getDepart());
+                    dep.SetDepart(obj.getDepart());
                 } catch (NullPointerException e) {
+
                 }
             }
             xmlDecoder.close();
         } catch (java.io.EOFException eofe) {
-            System.out.println("????? ???? ?????????? ??? ???");
+            System.out.println("File is empty");
+        } catch (FileNotFoundException e) {
+            System.out.println("There are not file" + filename);
+            System.exit(0);
+        } catch (java.util.zip.ZipException e) {
+            System.out.println(filename + " not in GZIP format");
+            System.exit(0);
         }
     }
 
