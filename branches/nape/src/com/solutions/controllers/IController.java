b4814716b579ed804dd5f141611cac2fa7cd0ed4
alekseyportnow@gmail.com <alekseyportnow@gmail.com@7159ea72-5042-624f-90be-b02e59218691>
2012-04-25 12:00:43 +0000

    Исправлена ошибка имплементирования

--- branches/nape/src/com/solutions/controllers/IController.java
+++ branches/nape/src/com/solutions/controllers/IController.java
@@ -7,5 +7,5 @@ import com.solutions.views.IView;
 public interface IController<TModel extends IModel, TView extends IView> {
     void setModel(TModel model);
     void setView(TView view);
-    void handleEvent(Event event);
+    void handleEvent(Event event)throws IOException;
 }
