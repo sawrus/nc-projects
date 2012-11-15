867c6743658cb897983a87e1389b058e3c400331
sawrus@gmail.com <sawrus@gmail.com@7159ea72-5042-624f-90be-b02e59218691>
2012-11-15 11:48:21 +0000

    cleanup

--- trunk/com/games/engine/events/impl/FakeEvent.java
+++
@@ -1,7 +0,0 @@
-package com.games.engine.events.impl;
-
-import com.games.engine.events.IEvent;
-
-public enum FakeEvent implements IEvent{
-    FAKE_EVENT
-}
--- trunk/com/solutions/samples/mvc/models/IModel.java
+++
@@ -1,9 +0,0 @@
-package com.solutions.samples.mvc.models;
-
-import com.solutions.samples.mvc.entities.IEntity;
-
-public interface IModel<TEntity extends IEntity> {
-    void clear();
-    TEntity getEntity();
-}
-
